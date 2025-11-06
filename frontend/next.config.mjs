4ff2871d9c7f66278e4df74a6b93b6a7f92b458a
Daniel De Vos <daniel@altfawesome.com>
2025-11-06 21:15:46 +0100

    Simplify Next.js config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
