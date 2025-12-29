3d3add74384a068f2d3b6e8fd1e920cbe67ca2cd
Daniel De Vos <daniel@altfawesome.com>
2025-12-29 18:22:31 +0100

    Fix TypeScript errors in sector-page API files
    - Add 'as any' type assertion to controllers and services
    - Matches pattern used in testimonial and other content types

--- cms/src/api/sector-page/controllers/sector-page.ts
+++ cms/src/api/sector-page/controllers/sector-page.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::sector-page.sector-page');
+export default factories.createCoreController('api::sector-page.sector-page' as any);
--- cms/src/api/sector-page/services/sector-page.ts
+++ cms/src/api/sector-page/services/sector-page.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::sector-page.sector-page');
+export default factories.createCoreService('api::sector-page.sector-page' as any);
